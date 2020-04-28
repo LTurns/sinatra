13a59bf053aa77731be9d577493faf1095b57ee7
LTurns <lizzieturney@gmail.com>
2020-04-28 13:03:39 +0100

    adding form params

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'shotgun'
+#set :session_secret, "secret_session"
 
 get '/' do
   "Hello World"
@@ -12,6 +14,13 @@ get '/secret/yo' do
   "and again"
 end
 
-get '/secret/yo/bye' do
-  "bye"
+get '/random-cat' do
+ @name = ["Lizzie", "Tom", "Jack"].sample
+ erb(:index)
+end
+
+get '/named-cat' do
+ p params
+ p @name = params[:name]
+ erb(:index)
 end
